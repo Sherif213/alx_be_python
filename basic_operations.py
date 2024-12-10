6fe5987bb23bff2d0b2749542f744f90d64967d9
Serif <elashrySerif@gmail.com>
2024-12-10 08:56:46 +0300

    initial commit

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum1 = number1 + number2
+diff = number2 - number1
+
+multiply = number1 * number2
+
+print("Addition of "+ number1 + " and "+ number2 + " is " + sum)
+print("Subtraction of "+ number1 + " and "+ number2 + " is " + diff)
+print("Multiplication of "+ number1 + " and "+ number2 + " is " + multiply)
