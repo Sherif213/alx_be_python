19adfe87b77cdf2e1fe2271d6603a79e2a740e1d
Sherif213 <shouldtheone@gmail.com>
2025-01-07 16:22:33 +0300

    Add arithmetic operations module and main execution script

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(num1,num2,operation):
+    match(operation):
+        case 'add':
+            return num1+num2
+        case 'subtract':
+            return num2-num1
+        case 'multiply':
+            return num1*num2
+        case 'divide':
+            return num2/num1
+        
