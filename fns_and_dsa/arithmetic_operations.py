b8765de76034f0db7da0375e16b0a841a793d71f
Sherif213 <shouldtheone@gmail.com>
2025-01-07 16:41:59 +0300

    Fix arithmetic operation logic in perform_operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,10 +2,10 @@ def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
-        return num2 - num1
+        return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        return num2 + num1
+        return num1 / num2
 
         
