a089e1d8921716026300d6f13fb421f962af7ba1
Sherif213 <shouldtheone@gmail.com>
2025-01-07 16:41:05 +0300

    Replace match-case structure with if-elif statements in perform_operation function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1, num2, operation):
-    match(operation):
-        case 'add':
-            return num1+num2
-        case 'subtract':
-            return num2-num1
-        case 'multiply':
-            return num1*num2
-        case 'divide':
-            return num2/num1
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num2 - num1
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        return num2 + num1
+
         
