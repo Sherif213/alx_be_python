f76c971b7f4c8656bcbb5bcf62f2479b4530a9e3
Sherif213 <shouldtheone@gmail.com>
2025-01-08 12:04:21 +0300

    Add datetime display and temperature conversion functionalities

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+# import datetime
+
+# def display_current_datetime():
+#     current_date = datetime.datetime.now()
+#     print(current_date)
+    
+# def calculate_future_date(day):
+#     future_date = (datetime.datetime.now() + datetime.timedelta(days=day)).date()
+    
+#     print(future_date)
+    
+# display_current_datetime()
+# calculate_future_date(10)
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,17 @@
+# FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+# CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+# def convert_to_celsius(fahrenheit):
+#     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+# def convert_to_fahrenheit(celsius):
+#     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+# temp = int(input("Enter the temperature to convert: "))
+# c_f = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+# if c_f == 'F':
+#     print(f"{temp}°F is {convert_to_celsius(temp)}°C")
+# elif c_f == 'C':
+#     print(f"{temp}°C is {convert_to_fahrenheit(temp)}°F")
+# else:
+#     print("Invalid input")
