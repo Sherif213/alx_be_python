52355086601d96da12245514631bfdfec6b7aabe
Sherif213 <shouldtheone@gmail.com>
2025-01-09 00:11:39 +0300

    Changes in the import

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,12 @@
-import datetime
+from datetime import datetime
+from datetime import timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(current_date)
     
 def calculate_future_date(day):
-    future_date = (datetime.datetime.now() + datetime.timedelta(days=day)).date()
+    future_date = (datetime.now() + timedelta(days=day)).date()
     
     print(future_date)
     
