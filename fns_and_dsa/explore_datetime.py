beea92cf29384f8a388b713f6b21b81fb1707aac
Sherif213 <shouldtheone@gmail.com>
2025-01-09 00:37:17 +0300

    Update future date calculation to return a datetime object and format output for display

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,9 +6,9 @@ def display_current_datetime():
     return current_date
     
 def calculate_future_date(day):
-    future_date = (datetime.now() + timedelta(days=day)).strftime("%Y-%m-%d")
+    future_date = (datetime.now() + timedelta(days=day))
     
-    return future_date
+    return future_date.strftime("%Y-%m-%d")
 print(f"Current date and time: {display_current_datetime()}")
 days = int(input("Enter the number of days to add to the current date: "))
 print(f"Future date: {calculate_future_date(days)}")
