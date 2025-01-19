b5c84bac585e1942cec4a4f360696155dca93132
Sherif213 <shouldtheone@gmail.com>
2025-01-19 18:04:12 +0300

    Add error handling for non-numeric inputs in safe_divide function

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,5 +1,9 @@
 def safe_divide(numerator, denominator):
     try:
+        float(numerator)/float(denominator)
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+    try:
         numerator/denominator
     except ZeroDivisionError:
         print(f"Error: Cannot divide by zero.")
