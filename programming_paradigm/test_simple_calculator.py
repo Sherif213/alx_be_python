c0b1efa96c7451832f935b1b3664580d76f59838
Sherif213 <shouldtheone@gmail.com>
2025-01-19 18:23:22 +0300

    Rename multiplication and division test methods for consistency

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -18,12 +18,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5,5),0)
         self.assertEqual(self.calc.subtract(0,5),-5)
         
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(10,5),50)
         self.assertEqual(self.calc.multiply(0,5),0)
         self.assertEqual(self.calc.multiply(1,1),1)
         
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,0),None)
         self.assertEqual(self.calc.divide(0,5),0)
         self.assertEqual(self.calc.divide(1,1),1)
