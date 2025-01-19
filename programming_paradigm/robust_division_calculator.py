59868d8eac4b46c7c8048b8f99c73956a08ae224
Sherif213 <shouldtheone@gmail.com>
2025-01-19 18:07:13 +0300

    Refactor safe_divide function to return error messages and results instead of printing

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,8 +2,10 @@ def safe_divide(numerator, denominator):
     try:
         float(numerator)/float(denominator)
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
     try:
         numerator/denominator
     except ZeroDivisionError:
-        print(f"Error: Cannot divide by zero.")
+        return f"Error: Cannot divide by zero."
+        
+    return f"The result of the division is {numerator/denominator}"
